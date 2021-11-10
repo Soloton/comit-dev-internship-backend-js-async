2a3e998b4f67ebf6f01901bcd83620056f63b25c
Mikhail Arkhipov <arkhipov.ms@gmail.com>
2021-11-10 19:13:29 +0300

    feat(jsasync-promises): Replacing with promises

--- task002/app.js
+++ task002/app.js
@@ -2,30 +2,40 @@ const fs = require("fs");
 const Mustache = require("mustache");
 const path = require("path");
 
-function createHtml(dataPath, templatePath, outputPath, callback) {
-  fs.readFile(dataPath, (errData, textData) => {
-    if (errData) {
-      callback(errData);
-    } else {
-      fs.readFile(templatePath, (errTemplate, textTemplate) => {
-        if (errTemplate) {
-          callback(errTemplate);
-        } else {
-          let template = textTemplate.toString();
-          let data = JSON.parse(textData.toString());
-          const rendered = Mustache.render(template, data, {
-            method:
-              "<strong>{{name}}</strong>" +
-              "(<ul class='param'>{{#param}}{{> field}}{{/param}}</ul>)" +
-              "{{#return}}: {{.}}{{/return}};<br/>\n",
-            field:
-              "<li><span>{{name}}</span>{{#class}}: {{.}}{{/class}}</li>\n",
-          });
-          fs.writeFile(outputPath, rendered, callback);
-        }
-      });
-    }
+function template(bufferTemplate, bufferData, outputPath) {
+  let sTemplate = bufferTemplate.toString();
+  let data = JSON.parse(bufferData.toString());
+  const rendered = Mustache.render(sTemplate, data, {
+    method:
+      "<strong>{{name}}</strong>" +
+      "(<ul class='param'>{{#param}}{{> field}}{{/param}}</ul>)" +
+      "{{#return}}: {{.}}{{/return}};<br/>\n",
+    field: "<li><span>{{name}}</span>{{#class}}: {{.}}{{/class}}</li>\n",
   });
+  return fs.promises.writeFile(outputPath, rendered);
+}
+
+function createHtml(dataPath, templatePath, outputPath, callback) {
+  let bufferData = "";
+  let bufferTemplate = "";
+  fs.promises
+    .readFile(dataPath)
+    .catch((errData) => callback(errData))
+    .then((data1) => {
+      bufferData = data1;
+    })
+    .then(() => fs.promises.readFile(templatePath))
+    .catch((errTemplate) => callback(errTemplate))
+    .then((data2) => {
+      bufferTemplate = data2;
+    })
+    .then(() => {
+      return template(bufferTemplate, bufferData, outputPath);
+    })
+    .catch((err) => {
+      callback(err);
+    })
+    .finally(() => callback());
 }
 
 createHtml(
